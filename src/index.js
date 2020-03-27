cdd78b27ffc975a05bb5a4e30f3fb556f192ba14
Dusankol <dusank89@gmail.com>
2020-03-27 20:38:02 +0100

    Routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+	
+serviceWorker.register();
